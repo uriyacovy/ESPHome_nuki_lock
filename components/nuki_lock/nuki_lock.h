85e61cadb3fa51a7594da1035154fff93f7e254b
Xavier Decuyper <hi@savjee.be>
2022-09-08 20:52:40 +0200

    Including the correct header

--- components/nuki_lock/nuki_lock.h
+++ components/nuki_lock/nuki_lock.h
@@ -5,6 +5,7 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/api/custom_api_device.h"
 
 #include "NukiBle.h"
 #include "NukiConstants.h"
